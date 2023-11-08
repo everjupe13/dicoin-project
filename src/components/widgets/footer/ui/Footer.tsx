393ba3d0bc6d3a8623c78e2f4992729ed8385404
Ilya Bakirov <everjupe13@gmail.com>
2023-11-08 19:42:08 +0300

    feat: rename footer component

---
+++ src/components/widgets/footer/ui/Footer.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const Footer: FC = () => {
+  return (
+    <footer className="pt-[80px]">
+      <div className="container"></div>
+    </footer>
+  )
+}
+
+export default Footer
