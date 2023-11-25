a250717e70c58fc8e13d9ad1db688bbed4fa7605
Ilya Bakirov <everjupe13@gmail.com>
2023-11-25 19:08:36 +0300

    feat: change default layout

--- src/layout/default.tsx
+++ src/layout/default.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom'
 
 import { Footer } from '@/components/widgets/footer'
 import { Header } from '@/components/widgets/header'
+import { Sidebar } from '@/components/widgets/sidebar'
 
 type Props = {
   children?: string | ReactElement | JSX.Element | JSX.Element[]
@@ -12,12 +13,10 @@ const DefaultLayout: FC<Props> = () => {
   return (
     <>
       <div className="relative">
-        <nav className="fixed bottom-0 left-0 top-0 z-[100] flex w-[240px] p-20">
-          <div className="w-full rounded-12 bg-[#1D232C]"></div>
-        </nav>
-        <nav className="fixed bottom-0 left-[240px] right-0 top-0 z-[100] flex h-[100px] p-20 pb-0 pl-0">
-          <Header></Header>
-        </nav>
+        <Sidebar />
+        <div className="fixed bottom-0 left-[240px] right-0 top-0 z-[100] flex h-[100px] p-20 pb-0 pl-0">
+          <Header />
+        </div>
         <main className="relatvie main z-[1] ml-[240px] mt-[100px]">
           <Outlet />
         </main>
