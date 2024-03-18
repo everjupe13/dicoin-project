6bfc7ca1ea90959d873ba6c62bcf60df6db8791f
Ilya Bakirov <everjupe13@gmail.com>
2024-03-19 02:17:08 +0300

    feat: add new fields at api data

--- src/logic/data/bill/types.ts
+++ src/logic/data/bill/types.ts
@@ -1,7 +1,10 @@
+import { IBillStatus } from '@/logic/domain/entity/bill/value-object/bill-status'
+
 export interface IBillApi {
   id: string
   name: string
   amount: number
   created_at: string
   updated_at: string
+  status: IBillStatus
 }
