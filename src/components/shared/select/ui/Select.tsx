e342d2f37bf4af671dcb8da1f53ffc9a8ed31f40
mnogodumal <raskol@duck.com>
2024-11-26 18:37:26 +0300

    fix select

--- src/components/shared/select/ui/Select.tsx
+++ src/components/shared/select/ui/Select.tsx
@@ -1,7 +1,14 @@
+import { SelectOption } from '@/app/types'
+
 export interface SelectProps {
-  options: { label: string; value: string }[]
+  options: SelectOption[]
   name: string
-  onChange: (value: string) => void
+  onChange: (value: string | string[]) => void // тут хз как использовать тайп SelectOption он же объект и как от
+  onFocus?: () => void
+  onBlur?: () => void
+  multiple?: boolean
+  disabled?: boolean
+  autocomplete?: boolean
 }
 
 export function Select({ options, name, onChange }: SelectProps) {
@@ -9,7 +16,7 @@ export function Select({ options, name, onChange }: SelectProps) {
     <div className="w-[250px] rounded-6 bg-[#3d3d3d] p-8">
       <select
         className="w-full bg-[#3d3d3d] outline-none"
-        name={name} // зачем?
+        name={name}
         onChange={e => onChange(e.target.value)}
       >
         {options.map(option => (
