c68339edbd12d6ac819081f6943dfe96e574fae2
mnogodumal <raskol@duck.com>
2025-01-13 17:09:35 +0300

    fix

---
+++ src/api/index.ts
@@ -0,0 +1,69 @@
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios'
+
+const basePath = 'http://176.53.163.14:3000/spending'
+
+export const apiClient = {
+  async get<T>(
+    url: string,
+    options?: AxiosRequestConfig
+  ): Promise<{ data: T | null; error: AxiosError | null }> {
+    return initApiClient('get', url, options)
+  },
+
+  async post<T>(
+    url: string,
+    options?: AxiosRequestConfig
+  ): Promise<{ data: T | null; error: AxiosError | null }> {
+    return initApiClient('post', url, options)
+  },
+
+  async put<T>(
+    url: string,
+    options?: AxiosRequestConfig
+  ): Promise<{ data: T | null; error: AxiosError | null }> {
+    return initApiClient('put', url, options)
+  },
+
+  async patch<T>(
+    url: string,
+    options?: AxiosRequestConfig
+  ): Promise<{ data: T | null; error: AxiosError | null }> {
+    return initApiClient('patch', url, options)
+  },
+
+  async delete<T>(
+    url: string,
+    options?: AxiosRequestConfig
+  ): Promise<{ data: T | null; error: AxiosError | null }> {
+    return initApiClient('delete', url, options)
+  }
+}
+
+async function initApiClient<T>(
+  method: 'get' | 'post' | 'put' | 'patch' | 'delete',
+  url: string,
+  options: AxiosRequestConfig | undefined
+): Promise<{ data: T | null; error: AxiosError | null }> {
+  const token = localStorage.getItem('x-auth-token')
+
+  const config: AxiosRequestConfig = {
+    ...options,
+    headers: {
+      ...options?.headers,
+      ...(token ? { Authorization: `${token}` } : {})
+    },
+    baseURL: basePath,
+    url,
+    method
+  }
+
+  try {
+    const response: AxiosResponse<T> = await axios.request(config)
+    return { data: response.data, error: null }
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return { data: null, error: error }
+    }
+    return { data: null, error: null }
+  }
+}
