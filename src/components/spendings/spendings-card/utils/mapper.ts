a9e613b3d0912701a2c0825256abf520572e99f3
mnogodumal <raskol@duck.com>
2025-01-22 08:54:00 +0300

    fix

--- src/components/spendings/spendings-card/utils/mapper.ts
+++ src/components/spendings/spendings-card/utils/mapper.ts
@@ -2,14 +2,12 @@ import { ISpendings } from '@/api/types/ISpendings'
 
 import { SpendingsCardProps } from '../ui/spendings-card'
 
-export function mapSpendings(
-  items?: Partial<ISpendings>[]
-): SpendingsCardProps[] {
+export function mapSpendings(items?: ISpendings[]): SpendingsCardProps[] {
   return (items || []).map(item => ({
-    id: item.id!,
-    name: item.name!,
-    withdrawalType: item.withdrawal_type!,
-    withdrawalDate: item.withdrawal_date!,
-    cost: item.cost!
+    id: item.id,
+    name: item.name,
+    withdrawalType: item.withdrawal_type,
+    withdrawalDate: item.withdrawal_date,
+    cost: item.cost
   }))
 }
