66625ba51c91bee8f6dfd9d6960e4bc83ad4c9b3
Ilya Bakirov <everjupe13@gmail.com>
2025-06-10 22:27:19 +0300

    feat: BREAKING tw4

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
 
   server: {
     host: '0.0.0.0',
